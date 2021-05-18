bf9843adcb7c6eda35ec91de17c4b6da623996ca
keza1brenda <keza1brenda@gmail.com>
2021-05-18 22:24:45 +0200

    add admin activity

---
+++ app/src/main/java/com/example/personalchef/AdminActivity.java
@@ -0,0 +1,24 @@
+package com.example.personalchef;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+
+    public void logoutAdmin(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+}
--- app/src/main/java/com/example/personalchef/DashboardOneActivity.java
+++ app/src/main/java/com/example/personalchef/DashboardOneActivity.java
@@ -2,7 +2,12 @@ package com.example.personalchef;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DashboardOneActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class DashboardOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_one);
+
+        Button logout = findViewById(R.id.logoutBtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
