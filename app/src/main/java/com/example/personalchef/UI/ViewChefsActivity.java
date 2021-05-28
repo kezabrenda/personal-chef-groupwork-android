5d7b244612c30160180e5316a5c3177e429ad5f2
keza1brenda <keza1brenda@gmail.com>
2021-05-28 15:17:18 +0200

    add jeanine's tasks

--- app/src/main/java/com/example/personalchef/UI/ViewChefsActivity.java
+++ app/src/main/java/com/example/personalchef/UI/ViewChefsActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.personalchef.R;
+import com.example.personalchef.booking.BookingActivity;
 import com.example.personalchef.databinding.ActivityViewChefsBinding;
 
 public class ViewChefsActivity extends AppCompatActivity {
     ActivityViewChefsBinding binding;
+    Button bookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,14 @@ public class ViewChefsActivity extends AppCompatActivity {
             binding.chefSpecialty.setText(chefSpeciality);
             binding.hourlyRate.setText(hourlyRate);
             binding.chefImage1.setImageResource(imageId);
-
         }
+        bookButton = findViewById(R.id.bookButton);
+
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BookingActivity.class));
+            }
+        });
     }
 }
